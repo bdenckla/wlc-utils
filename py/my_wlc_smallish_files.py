8147012b1588930a12ba0ee4fb030cd4d9f22080
Ben Denckla <bdenckla@alum.mit.edu>
2024-02-23 18:19:32 -0500

    redo smallish files with fixed definition

--- py/my_wlc_smallish_files.py
+++ py/my_wlc_smallish_files.py
@@ -6,32 +6,59 @@ import my_open
 def write(tdir, wlc_id, parsed):
     out_path_for_header = f'{tdir}/out/{wlc_id}/{wlc_id}_ps.0header.json'
     my_open.json_dump_to_file_path(parsed['header'], out_path_for_header)
-    sfc = 0  # smallish file count
-    bkids_icf = {}  # icf: in current [output] file
-    verses_icf = []  # icf: in current [output] file
+    chunks = _chunks_init()
     for verse in parsed['verses']:
-        bk_of_verse = _bk_of_verse(verse)
-        # If we're already bigger than we'd like to be
-        # and we're starting a new book ...
-        if len(verses_icf) > 1500 and bk_of_verse not in bkids_icf:
-            _write_smallish_file(tdir, wlc_id, sfc, bkids_icf, verses_icf)
-            sfc += 1
-            bkids_icf = {}
-            verses_icf = []
-        else:
-            bkids_icf[bk_of_verse] = True
-            verses_icf.append(verse)
-    if verses_icf:
-        _write_smallish_file(tdir, wlc_id, sfc, bkids_icf, verses_icf)
+        chunk_str = _CHUNK_STR_FOR_BKID[_bkid_of_verse(verse)]
+        chunks[chunk_str].append(verse)
+    for chunk_str, chunk_verses in chunks.items():
+        _write_smallish_file(tdir, wlc_id, chunk_str, chunk_verses)
 
 
-def _bk_of_verse(verse):
+def _write_smallish_file(tdir, wlc_id, chunk_str, chunk_verses):
+    chunk_idx = _CHUNK_IDX_FOR_CHUNK_STR[chunk_str]
+    idx_and_bkids = f'{chunk_idx:02}_{chunk_str}'
+    out_path = f'{tdir}/out/{wlc_id}/{wlc_id}_ps.1verses_{idx_and_bkids}.json'
+    my_open.json_dump_to_file_path(chunk_verses, out_path)
+
+
+def _bkid_of_verse(verse):
     return verse['bcv'][:2]
 
 
-def _write_smallish_file(tdir, wlc_id, sfc, bkids_icf, verses_icf):
-    str_for_bkids = ''.join(bkids_icf)  # e.g. 'exlv'
-    str_for_sfc = f'{sfc:02}'  # e.g. '02' (smallish file count)
-    str_for_sfc_and_bkids = str_for_sfc + '_' + str_for_bkids
-    out_path = f'{tdir}/out/{wlc_id}/{wlc_id}_ps.1verses_{str_for_sfc_and_bkids}.json'
-    my_open.json_dump_to_file_path(verses_icf, out_path)
+def _chunk_str_for_bkid():
+    out = {}
+    for bkid_tuple in _CHUNK_DEFINTIONS:
+        for bkid in bkid_tuple:
+            out[bkid] = ''.join(bkid_tuple)
+    return out
+
+
+def _chunk_idx_for_chunk_str():
+    out = {}
+    for chunk_idx, bkid_tuple in enumerate(_CHUNK_DEFINTIONS):
+        out[''.join(bkid_tuple)] = chunk_idx
+    return out
+
+
+def _chunks_init():
+    out = {}
+    for bkid_tuple in _CHUNK_DEFINTIONS:
+        out[''.join(bkid_tuple)] = []
+    return out
+
+
+_CHUNK_DEFINTIONS = [
+    ('gn',),
+    ('ex', 'lv'),
+    ('nu', 'dt'),
+    ('js', 'ju', '1s'),
+    ('2s', '1k'),
+    ('2k', 'is'),
+    ('je', 'ek'),
+    ('ho', 'jl', 'am', 'ob', 'jn', 'mi', 'na', 'hb', 'zp', 'hg', 'zc', 'ma', '1c'),
+    ('2c', 'ps'),
+    ('jb', 'pr'),
+    ('ru', 'ca', 'ec', 'lm', 'es', 'da', 'er', 'ne')
+]
+_CHUNK_STR_FOR_BKID = _chunk_str_for_bkid()
+_CHUNK_IDX_FOR_CHUNK_STR = _chunk_idx_for_chunk_str()
