725a9478c1f0b5498bdd9db66a0369a10ca70fec
Ben Denckla <bdenckla@alum.mit.edu>
2024-02-23 16:22:39 -0500

    get kqparse seemingly working

--- py/my_wlc_kqparse.py
+++ py/my_wlc_kqparse.py
@@ -3,12 +3,41 @@ import my_wlc_utils
 
 def kqparse(parsed):
     kqparsed = {'verses': []}
+    ketiv_stack = []
+    qere_stack = []
     for verse in parsed['verses']:
         bcv = verse['bcv']
         kqverse = {'bcv': bcv, 'kqvels': []}
         kqparsed['verses'].append(kqverse)
+        assert not ketiv_stack
+        assert not qere_stack
         for velsod in verse['vels']:
-            veldic = my_wlc_utils.velsod_to_veldic(velsod)
-            kqvelsod = my_wlc_utils.veldic_to_velsod(veldic)
-            kqverse['kqvels'].append(kqvelsod)
+            word = _word(velsod)
+            if word and word.startswith('**'):
+                qere_stack.append(word)
+                continue
+            if word and word.startswith('*'):
+                ketiv_stack.append(word)
+                continue
+            _complete(kqverse['kqvels'], ketiv_stack, qere_stack)
+            ketiv_stack = []
+            qere_stack = []
+            kqverse['kqvels'].append(velsod)
+        _complete(kqverse['kqvels'], ketiv_stack, qere_stack)
+        ketiv_stack = []
+        qere_stack = []
     return kqparsed
+
+
+def _complete(io_kqvels, ketiv_stack, qere_stack):
+    if ketiv_stack and qere_stack:
+        io_kqvels.append({'kq': (ketiv_stack, qere_stack)})
+    else:
+        assert not ketiv_stack and not qere_stack
+
+
+
+def _word(velsod):
+    if isinstance(velsod, str):
+        return velsod
+    return velsod.get('word')
