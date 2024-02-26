1936e09262cec0c49738afdd209e8cb7e2f91b7d
Ben Denckla <bdenckla@alum.mit.edu>
2024-02-26 09:36:19 -0500

    1st pass at accent translation to Unicode

--- py/my_uword.py
+++ py/my_uword.py
@@ -1,10 +1,20 @@
+import re
 import my_hebrew_points as hpo
 import my_hebrew_accents as ha
 import my_hebrew_punctuation as hpu
 
 
 def uword(mcword: str):
-    return mcword.translate(_TRANSLATION_TABLE)
+    translated = mcword.translate(_TRANSLATION_TABLE)
+    pattern = r'\d\d'
+    subbed = re.sub(pattern, digits_replacement, translated)
+    return subbed
+
+
+def digits_replacement(matchobj):
+    digit_pair = matchobj.group()
+    return _ACCENTS[digit_pair]
+
 
 _TRANSLATION_TABLE = str.maketrans({
     '/': None,
@@ -52,13 +62,13 @@ _ACCENTS = {
     '04': ha.TEL_Q,
     '05': hpu.PAS,
     '10': ha.YETIV,
-    '13': ha.TIP,  # dehi or tipha             II.9
+    '13': ha.TIP,
     '11': ha.GER_M,
     '12': ha.GER_2,  # garshayim (preposed)
-    '14': ha.TEL_G,  # telisha gedolah
+    '14': ha.TEL_G,
     '24': ha.TEL_Q,  # 4 uses as stress-helper to acc04 (24\S+04); 1 use as non-stress-helper
     '33': ha.PASH,  # >3800 uses as stress-helper to acc03 (33\S+03); number of uses as non-stress-helper is unknown (may be 0)
-    '44': ha.TEL_G,  # 1 use as stress-helper to acc14 ([^a-z]14\S+44); 3 non-stress-helper uses
+    '44': ha.TEL_G,  # 1 use as stress-helper to acc14 ([^a-z]14\S+44); 3 uses as non-stress-helper
     '52': '\N{HEBREW MARK UPPER DOT}',
     '60': ha.OLE,
     '61': ha.GER,
@@ -68,7 +78,7 @@ _ACCENTS = {
     '65': ha.SHAL,
     '80': ha.ZAQEF_Q,
     '81': ha.REV,
-    '82': ha.ZARQA_SH,  # 2 uses as stress-helper to acc02 (82\S+02); many uses as tsinnorit
+    '82': ha.ZARQA_SH,  # 2 uses as stress-helper to acc02 (82\S+02); >200 uses as tsinnorit
     '83': ha.PAZER,
     '84': ha.QARNEY,  # aka pazer gadol
     '85': ha.ZAQEF_G,
@@ -86,3 +96,5 @@ _ACCENTS = {
     '94': ha.DARGA,
     '95': hpo.METEG, # right ("early")
 }
+# uword('A12C34')
+# uword('A1234')
