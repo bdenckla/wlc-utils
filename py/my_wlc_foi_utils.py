a4325f7424ef9b19d99f4c85f0b04078244ae1c9
Ben Denckla <bdenckla@alum.mit.edu>
2024-12-09 22:04:36 -0500

    don't fois next to verses files
    this is to try to avoid repeating the mistake I just made,
    which was to count search "hits" in the directory with verses

--- py/my_wlc_foi_utils.py
+++ py/my_wlc_foi_utils.py
@@ -33,7 +33,7 @@ def _flexdump(fois, tdir, wlc_id, suffix=''):
 
 
 def _flexpath(tdir, wlc_id, suffix=''):
-    return f'{tdir}/out/{wlc_id}{suffix}/{wlc_id}_ps.fois.json'
+    return f'{tdir}/out/{wlc_id}{suffix}_ps.fois.json'
 
 
 def _init():
