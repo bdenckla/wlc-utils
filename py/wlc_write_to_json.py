bdbb2f21fd683fbfa8674d3bb3a89d5e0aea4cb3
Ben Denckla <bdenckla@alum.mit.edu>
2025-04-04 11:39:19 -0400

    qualify some file.py names with mdc and/or uni

--- py/wlc_write_to_json.py
+++ py/wlc_write_to_json.py
@@ -5,7 +5,7 @@ import py.wlc_read_and_parse_uni as rp_uni
 import py.wlc_kqparse as kqparse
 import py.wlc_foi_utils as foi_utils
 import py.wlc_smallish_files as smallish_files
-import py.wlc_convert_p_mcd_to_p_uni as mu
+import py.wlc_convert_mcd_to_uni as mu
 import py.wlc_release_info as ri
 
 
