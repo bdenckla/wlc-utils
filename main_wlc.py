6a8520abbb2c3c4864ab46aed6c66d95edae33d6
Ben Denckla <bdenckla@alum.mit.edu>
2025-04-04 12:05:10 -0400

    groundwork phase 3 for uu compare

--- main_wlc.py
+++ main_wlc.py
@@ -6,13 +6,18 @@ import py.wlc_compare_mdc_with_mdc as mm
 import py.wlc_compare_uni_with_uni as uu
 
 
-def _wu_out_path(wlc_id):
-    return f"{_TDIR}/out/diff_{wlc_id}_uxlc.json"
+def _mx_out_path(wlc_id):
+    return f"{_TDIR}/out/diff_mx_{wlc_id}_uxlc.json"
 
 
-def _ww_out_path(ww_diff_ids):
+def _mm_out_path(ww_diff_ids):
     wlc_ida, wlc_idb = ww_diff_ids
-    return f"{_TDIR}/out/diff_{wlc_ida}_{wlc_idb}.json"
+    return f"{_TDIR}/out/diff_mm_{wlc_ida}_{wlc_idb}.json"
+
+
+def _uu_out_path(ww_diff_ids):
+    wlc_ida, wlc_idb = ww_diff_ids
+    return f"{_TDIR}/out/diff_uu_{wlc_ida}_{wlc_idb}.json"
 
 
 def _out_path(wlc_id, suffix):
@@ -30,9 +35,10 @@ def main():
     p321mcd, p321mcdu = wlc_write_to_json.write(path_info, "2025-03-21-mcd")
     p420mcd, _u = wlc_write_to_json.write(path_info, "wlc420")
     p422mcd, _u = wlc_write_to_json.write(path_info, "wlc422")
-    mx.compare(p420mcd, _UXLC_BOOKS_DIR, _wu_out_path)
-    mm.compare(p420mcd, p422mcd, _ww_out_path)
-    mm.compare(p420mcd, p321mcd, _ww_out_path)
+    # uu.compare(p321mcdu, p321uni, _uu_out_path)
+    mx.compare(p420mcd, _UXLC_BOOKS_DIR, _mx_out_path)
+    mm.compare(p420mcd, p422mcd, _mm_out_path)
+    mm.compare(p420mcd, p321mcd, _mm_out_path)
 
 
 _TDIR = "../wlc-utils-io"
