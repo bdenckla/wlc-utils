8205ce5a111069da0c63dc75361e62a751861f47
Ben Denckla <bdenckla@alum.mit.edu>
2026-06-03 17:10:17 -0400

    links to FOIs

--- py/accgram/research_tms_report_open_issues.py
+++ py/accgram/research_tms_report_open_issues.py
@@ -9,6 +9,25 @@ _ITEM_FOI_NON_REVIA = (
     " while the other 12 do not?"
 )
 
+_FOI_NON_REVIA_CATEGORY_LINKS = (
+    (
+        "⅃-leg...non-revia ((paz)) with 0 intervening",
+        "https://bdenckla.github.io/MAM-with-doc/foi/foi-pasoleg-1.html#intro-%E2%85%83-leg...non-revia%C2%ABspace%C2%BB((paz))%C2%ABspace%C2%BBwith%C2%ABspace%C2%BB0%C2%ABspace%C2%BBintervening",
+    ),
+    (
+        "⅃-leg...non-revia ((tev)) with 2 (qa,da) intervening",
+        "https://bdenckla.github.io/MAM-with-doc/foi/foi-pasoleg-1.html#intro-%E2%85%83-leg...non-revia%C2%ABspace%C2%BB((tev))%C2%ABspace%C2%BBwith%C2%ABspace%C2%BB2%C2%ABspace%C2%BB(qa,da)%C2%ABspace%C2%BBintervening",
+    ),
+    (
+        "⅃-leg...non-revia (ge) with 1 qa intervening",
+        "https://bdenckla.github.io/MAM-with-doc/foi/foi-pasoleg-1.html#intro-%E2%85%83-leg...non-revia%C2%ABspace%C2%BB(ge)%C2%ABspace%C2%BBwith%C2%ABspace%C2%BB1%C2%ABspace%C2%BBqa%C2%ABspace%C2%BBintervening",
+    ),
+    (
+        "⅃-leg...non-revia (p) with 1 (mah) intervening",
+        "https://bdenckla.github.io/MAM-with-doc/foi/foi-pasoleg-1.html#intro-%E2%85%83-leg...non-revia%C2%ABspace%C2%BB(p)%C2%ABspace%C2%BBwith%C2%ABspace%C2%BB1%C2%ABspace%C2%BB(mah)%C2%ABspace%C2%BBintervening",
+    ),
+)
+
 _ITEM_ZARQA_ON_LAMED = (
     "zarqa on lamed (accent 82 preceding lamed):"
     " WLC encodes 12 such cases with accent 82 placed before the lamed rather than after it."
@@ -16,8 +35,16 @@ _ITEM_ZARQA_ON_LAMED = (
 )
 
 
+def _build_foi_non_revia_item() -> tuple[object, ...]:
+    category_links = tuple(
+        wlc_utils_html.anchor(label, {"href": href})
+        for label, href in _FOI_NON_REVIA_CATEGORY_LINKS
+    )
+    return (_ITEM_FOI_NON_REVIA, wlc_utils_html.unordered_list(category_links))
+
+
 def build_open_issues_section() -> tuple[object, ...]:
-    list_items = (_ITEM_FOI_NON_REVIA, _ITEM_ZARQA_ON_LAMED)
+    list_items = (_build_foi_non_revia_item(), _ITEM_ZARQA_ON_LAMED)
     return (
         wlc_utils_html.heading_level_2("Open Issues"),
         wlc_utils_html.unordered_list(list_items),
