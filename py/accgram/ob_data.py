6b914d459154052e4de7a009c0bfc3bbcad51020
Ben Denckla <bdenckla@alum.mit.edu>
2026-06-06 09:48:42 -0400

    fill in some ob_data, mainly wlc_focus

--- py/accgram/ob_data.py
+++ py/accgram/ob_data.py
@@ -2,136 +2,142 @@
 
 from __future__ import annotations
 
+from accgram import xx_data
+
 
 def get_structured_text() -> dict[str, dict[str, object]]:
     return STRUCTURED_TEXT_BY_REF
 
 
+_SOMEWHERE = "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc."
 STRUCTURED_TEXT_BY_REF: dict[str, dict[str, object]] = {
     "1c 1:53": {
         "wlc_focus": "אל֣וף",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "1k 6:3": {
         "wlc_focus": "עשר֣ים",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2022.12.07%20-%20Changes/2022.12.07%20-%20Changes.xml?2022.08.31-10",
     },
     "1k 8:11": {
         "wlc_focus": "מפנ֥י",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2022.04.01%20-%20Changes/2022.04.01%20-%20Changes.xml?2021.11.21-1",
     },
     "1k 20:25": {
-        "wlc_focus": "",
+        "wlc_focus": "וס֣וס",
         "st-summary": "",
     },
     "1s 14:3": {
-        "wlc_focus": "",
-        "st-summary": "",
+        "wlc_focus": "על֜י",
+        "st-summary": xx_data.non_revia_summary("geresh"),
     },
     "1s 14:47": {
-        "wlc_focus": "",
-        "st-summary": "",
+        "wlc_focus": "ובאד֜ום",
+        "st-summary": xx_data.non_revia_summary("geresh"),
     },
     "2c 7:5": {
         "wlc_focus": "הבק֗ר",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2024.04.01%20-%20Changes/2024.04.01%20-%20Changes.xml?2023.09.16-3",
     },
     "2c 8:10": {
         "wlc_focus": "שר֤י",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2024.04.01%20-%20Changes/2024.04.01%20-%20Changes.xml?2023.09.16-4",
     },
     "2c 24:27": {
         "wlc_focus": "י֧ר֞ב",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "2c 25:1": {
         "wlc_focus": "ועשר֣ים",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2024.04.01%20-%20Changes/2024.04.01%20-%20Changes.xml?2023.09.16-13",
     },
     "2k 18:17": {
-        "wlc_focus": "",
-        "st-summary": "",
+        "wlc_focus": "תרת֥ן",
+        "st-summary": _SOMEWHERE + " " + xx_data.non_revia_summary("geresh", "Also, the"),
     },
     "2s 6:20": {
         "wlc_focus": "לעינ֨י",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2022.12.07%20-%20Changes/2022.12.07%20-%20Changes.xml?2022.08.30-17",
     },
     "2s 11:11": {
         "wlc_focus": "אדנ֨י",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "2s 13:32": {
-        "wlc_focus": "",
-        "st-summary": "",
+        "wlc_focus": "דו֜ד",
+        "st-summary": xx_data.non_revia_summary("geresh"),
     },
     "2s 23:9": {
-        "wlc_focus": "",
+        "wlc_focus": "בפלשתים",
         "st-summary": "",
         "uxlc_change": "https://tanach.us/Changes/2021.10.19%20-%20Changes/2021.10.19%20-%20Changes.xml?2021.08.07-3",
     },
     "dt 10:15": {
         "wlc_focus": "הזה׃",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "dt 12:2": {
         "wlc_focus": "רענן׃",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "dt 23:18": {
         "wlc_focus": "ישראל׃",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "dt 24:10": {
         "wlc_focus": "ברעך",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "dt 31:7": {
         "wlc_focus": "ישרא֘ל",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2021.04.01%20-%20Changes/2021.04.01%20-%20Changes.xml?2020.12.06-9",
     },
     "ec 12:14": {
-        "wlc_focus": "",
-        "st-summary": "",
+        "wlc_focus": "כ֤י",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2023.10.19%20-%20Changes/2023.10.19%20-%20Changes.xml?2023.09.11-25",
     },
     "ek 9:2": {
-        "wlc_focus": "",
-        "st-summary": "",
+        "wlc_focus": "העלי֜ון",
+        "st-summary": xx_data.non_revia_summary("geresh"),
     },
     "ex 4:10": {
         "wlc_focus": "דברך",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2021.10.19%20-%20Changes/2021.10.19%20-%20Changes.xml?2021.08.07-4",
     },
     "ex 6:6": {
         "wlc_focus": "ישרא֘ל",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2021.04.01%20-%20Changes/2021.04.01%20-%20Changes.xml?2020.12.06-2",
     },
     "ex 28:1": {
-        "wlc_focus": "",
+        "wlc_focus": "את֔ו",
         "st-summary": "",
         "uxlc_change": "https://tanach.us/Changes/2022.04.01%20-%20Changes/2022.04.01%20-%20Changes.xml?2021.11.28-2",
+        "comment": (
+            "This is the example given in Goerwitz’s article."
+        ),
     },
     "ex 30:12": {
         "wlc_focus": "ישרא֘ל",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2021.04.01%20-%20Changes/2021.04.01%20-%20Changes.xml?2020.12.06-3",
     },
     "ex 38:12": {
         "wlc_focus": "עמודיה֥ם",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "gn 28:9": {
-        "wlc_focus": "",
-        "st-summary": "",
+        "wlc_focus": "אברה֜ם",
+        "st-summary": xx_data.non_revia_summary("geresh"),
     },
     "gn 32:24": {
         "wlc_focus": "",
@@ -139,11 +145,11 @@ STRUCTURED_TEXT_BY_REF: dict[str, dict[str, object]] = {
     },
     "ho 11:7": {
         "wlc_focus": "ירומם׃",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "is 13:7": {
         "wlc_focus": "ימס׃",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "is 45:1": {
         "wlc_focus": "",
@@ -151,12 +157,12 @@ STRUCTURED_TEXT_BY_REF: dict[str, dict[str, object]] = {
     },
     "je 26:5": {
         "wlc_focus": "דבר֨י",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2023.04.01%20-%20Changes/2023.04.01%20-%20Changes.xml?2022.12.10-28",
     },
     "je 28:2": {
         "wlc_focus": "שב֞רתי",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2023.04.01%20-%20Changes/2023.04.01%20-%20Changes.xml?2022.12.10-35",
     },
     "je 40:11": {
@@ -165,11 +171,11 @@ STRUCTURED_TEXT_BY_REF: dict[str, dict[str, object]] = {
     },
     "je 46:4": {
         "wlc_focus": "בכ֥ובע֑ים",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "je 51:9": {
         "wlc_focus": "רפ֣ינו",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "js 4:8": {
         "wlc_focus": "",
@@ -178,28 +184,28 @@ STRUCTURED_TEXT_BY_REF: dict[str, dict[str, object]] = {
     },
     "js 10:30": {
         "wlc_focus": "ישרא֘ל",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2021.04.01%20-%20Changes/2021.04.01%20-%20Changes.xml?2020.12.06-11",
     },
     "js 15:47": {
         "wlc_focus": "עז֥ה",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "ju 1:2": {
         "wlc_focus": "וי֣אמר",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "ju 11:24": {
         "wlc_focus": "אות֥ו",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "ju 21:16": {
         "wlc_focus": "ויאמר֨ו",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "lv 4:2": {
         "wlc_focus": "ישרא֘ל",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2021.04.01%20-%20Changes/2021.04.01%20-%20Changes.xml?2020.12.06-5",
         "other-goerwitz-item": "goerwitz-tms-msp-n.html#tmnu20v19",
         "comment": (
@@ -216,16 +222,16 @@ STRUCTURED_TEXT_BY_REF: dict[str, dict[str, object]] = {
     },
     "lv 25:20": {
         "wlc_focus": "נאכ֤֖ל",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2021.04.01%20-%20Changes/2021.04.01%20-%20Changes.xml?2020.12.22-2",
     },
     "lv 26:28": {
         "wlc_focus": "חטאתיכם׃",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
     "ne 2:10": {
         "wlc_focus": "ב֥א",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
         "uxlc_change": "https://tanach.us/Changes/2024.04.01%20-%20Changes/2024.04.01%20-%20Changes.xml?2023.09.14-3",
     },
     "ne 8:7": {
@@ -242,6 +248,6 @@ STRUCTURED_TEXT_BY_REF: dict[str, dict[str, object]] = {
     },
     "nu 27:9": {
         "wlc_focus": "לאחיו׃",
-        "st-summary": "Somewhere in the LC-BHS-WLC pipeline, $wlc_focus_desc appears rather than $diff_wlc_mam_desc.",
+        "st-summary": _SOMEWHERE,
     },
 }
